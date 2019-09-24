902e703b0a37d32a506e29f45b115405e5d2bf39
Jason Sonnichsen <Harliquinn@gmail.com>
2019-09-23 20:10:00 -0400

    Added Login Page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.scss";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 import { connect } from "react-redux";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
 
-function Login() {
+const Login = props => {
+  // console.log("Login", props);
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: ""
+  });
+  const [isLoading, setIsLoading] = useState(false);
+
+  //   console.log("Cred", credentials);
+  const handleChange = e => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const login = e => {
+    e.preventDefault();
+    setIsLoading(true);
+    axiosWithAuth()
+      .post("/login", credentials)
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+        props.history.push("/");
+      })
+      .catch(err => console.log(err));
+  };
+
   return (
-    <div className="login">
-      <h2>Login Form</h2>
+    <div>
+      <form onSubmit={login}>
+        <input
+          type="text"
+          name="username"
+          value={credentials.username}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={credentials.password}
+          onChange={handleChange}
+        />
+        <button>Log in</button>
+      </form>
+      <div className="loading">
+        {isLoading && (
+          <>
+            <h2>Loading...</h2>
+            <Loader type="Rings" color="red" height={80} width={80} />
+          </>
+        )}
+      </div>
     </div>
   );
-}
-
+};
 const mapStateToProps = state => {
   return {
     stateObj: state.Obj
