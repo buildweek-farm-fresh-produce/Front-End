c37d2f5ec3aadb3915f9b4f9cb4c53ec1ec952e2
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2019-09-24 12:27:48 +0100

    starting styling of sign up form

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,24 +1,57 @@
 import React from "react";
 import { withFormik, Form, Field } from "formik"; //withFormik is used to create a higher order component
-import Yup from "yup";
+import * as Yup from "yup";
+import styled from "styled-components";
 
-const SignUp = ({ values, handleChange }) => {
+const SyledMainDiv = styled.div`
+  margin: 10px auto;
+  border: 5px solid #a4a37a;
+  width: 600px;
+  background: white;
+  display: flex;
+  justify-content: space-between;
+  border-radius: 10px;
+`;
+const UserDiv = styled.div`
+  label {
+    display: flex;
+    flex-direction: row;
+  }
+  border: 2px solid red;
+
+  h2 {
+    border: 2px solid green;
+  }
+
+  input {
+    border: 2px solid red;
+    display: block;
+    margin: 20px auto;
+    line-height: 37px;
+    font-size: 1.7rem;
+  }
+`;
+
+const SignUp = ({ values, errors, touched, isSubmitting }) => {
   return (
-    <div>
+    <SyledMainDiv>
       <Form>
-        <div>
+        <UserDiv>
+          {touched.username && errors.username && <p>{errors.username}</p>}
           <label>
-            Username:
+            <h2>Username:</h2>
             <Field type="username" name="username" placeholder="Username" />
           </label>
-        </div>
+        </UserDiv>
         <div>
+          {touched.email && errors.email && <p>{errors.email}</p>}
           <label>
             Email:
             <Field type="email" name="email" placeholder="Email" />
           </label>
         </div>
         <div>
+          {touched.password && errors.password && <p>{errors.password}</p>}
           <label>
             Password:
             <Field type="password" name="password" placeholder="Password" />
@@ -44,9 +77,9 @@ const SignUp = ({ values, handleChange }) => {
           Consumer:
           <Field name="consumer" type="checkbox" checked={values.consumer} />
         </label>
-        <button>Submit</button>
+        <button disabled={isSubmitting}>Submit</button>
       </Form>
-    </div>
+    </SyledMainDiv>
   );
 };
 
@@ -70,7 +103,24 @@ const FormikSignUp = withFormik({
       consumer: consumer || false
     };
   },
-  handleSubmit(values) {
+  validationSchema: Yup.object().shape({
+    username: Yup.string().required("Username is required"),
+    email: Yup.string()
+      .email("Email is not valid")
+      .required("Email is required"),
+    password: Yup.string()
+      .min(9, "Password must be 9 characters or more")
+      .required("Password is required")
+  }),
+  handleSubmit(values, { resetForm, setSubmitting, setErrors }) {
+    setTimeout(() => {
+      if (values.email === "tolu@test.com") {
+        setErrors({ email: "The Email is taken" });
+      } else {
+        resetForm();
+      }
+      setSubmitting(false);
+    }, 2000);
     console.log(values);
   }
 })(SignUp);
