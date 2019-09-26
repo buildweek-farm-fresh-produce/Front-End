f015d952984584ee5ddfa683ccc339eaf8152ad1
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2019-09-26 13:22:36 +0100

    styling shop category

--- src/components/Shop/ShopList.js
+++ src/components/Shop/ShopList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { axiosWithAuth } from ".././../utils/axiosWithAuth";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 export const ShopList = () => {
   const [shopState, setShopState] = useState([]);
@@ -22,22 +23,66 @@ export const ShopList = () => {
   }, []);
   return (
     <div>
-      <h2>Shop List</h2>
-      <div>
+      <Header1Style>
+        <h2>Shop List Category</h2>
+      </Header1Style>
+
+      <ShopState className="test">
         {shopState.map(items => (
           <ShopDetails key={items.id} items={items} />
         ))}
-      </div>
+      </ShopState>
     </div>
   );
 };
 function ShopDetails({ items }) {
   const { id, name } = items;
   return (
-    <div>
+    <div className="Header2Style">
       <Link to={`/shop/${id}`}>
-        <h2>Name: {name}</h2>
+        <Header2Style>
+          <h3>{name}</h3>
+        </Header2Style>
       </Link>
     </div>
   );
 }
+
+const ShopState = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  width: 600px;
+  margin-left: 378px;
+`;
+
+const Header2Style = styled.h2`
+  margin: 10px auto;
+  border: 10px solid tomato;
+  width: 250px;
+  background: white;
+  display: flex;
+  justify-content: space-between;
+  border-radius: 10px;
+  font-size: 1.7rem;
+  h3 {
+    margin-left: 70px;
+    font-size: 1.7rem;
+    
+  }
+`;
+
+const Header1Style = styled.h2`
+  margin: 10px auto;
+  border: 10px solid tomato;
+  width: 600px;
+  background: #a4a37a;
+  display: flex;
+  justify-content: space-between;
+  border-radius: 10px;
+  h2 {
+    margin-left: 180px;
+    font-size: 1.7rem;
+    color: white;
+  }
+`;
